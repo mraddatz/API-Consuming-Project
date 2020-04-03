c004da36d7148cbafde823fa51c594087e6c3069
Martin Raddatz <martinraddatz@MacBook-Pro-de-Martin.local>
2020-04-02 23:22:05 -0300

    create app

--- djangoproject/settings.py
+++ djangoproject/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'djangoproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['rickmortyapp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
